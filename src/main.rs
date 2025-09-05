fbcea9c4128e7bfc47c2ec7aef987013f8897aa3
Arefin Ahmed <aa2376131@gmail.com>
2025-09-05 22:15:17 +0600

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+print_message("🐸".to_string()); // Correct: "🐸" (&str) is converted to String
+}
+
+fn print_message(s: String) { // Implicitly returns ()
+    // In Rust 2021 edition and later, "{s}" uses s as an implicit named argument.
+    // This line prints the content of the string 's' five times, concatenated.
+    println!("{s}{s}{s}{s}{s}");
+}
